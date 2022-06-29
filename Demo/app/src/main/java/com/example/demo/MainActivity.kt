8d7c91cce6cad67bcbf8968f0c9e1bbeab6a7ad7
Kardem-MESE <101044970+Kardem-MESE@users.noreply.github.com>
2022-06-29 09:18:35 +0300

    demo-continue

--- Demo/app/src/main/java/com/example/demo/MainActivity.kt
+++ Demo/app/src/main/java/com/example/demo/MainActivity.kt
@@ -4,16 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import androidx.activity.viewModels
-import androidx.core.widget.addTextChangedListener
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.demo.databinding.ActivityMainBinding
-import com.example.demo.databinding.CardTasarimiBinding
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.card_tasarimi.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -29,29 +27,43 @@ class MainActivity : AppCompatActivity() {
 
         setRecyclerview()
         setListeners()
-        resultcalculation()
+        //resultcalculation()
         viewModel.refreshData()
 
     }
-    private fun resultcalculation() {
+
+
+
+
+
+    /*private fun resultcalculation() {
         viewModel.sonuc.observe(this,{ s ->
             binding.textViewSonuc.text = s
         })
     }
+
+     */
     private fun setListeners() {
         binding.button3.setOnClickListener {
-            val s1 = Sekiller(4,"Elips")
+
+            val s1 = Sekiller(4,Textwatcher.text.toString())
             viewModel.addsekil(s1)
         }
         binding.Textwatcher.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
             }
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-
+                button3.isEnabled = s.toString().trim().isNotEmpty()
             }
             override fun afterTextChanged(s: Editable?) {
             }
         })
+
+
+
+
+
+        /*
         binding.buttonSquare.setOnClickListener {
             val enteredLength = binding.editTextSideLength.text.toString()
             val enteredHeight = binding.editTextHeight.text.toString()
@@ -62,7 +74,11 @@ class MainActivity : AppCompatActivity() {
             val enteredHeight = binding.editTextHeight.text.toString()
             viewModel.ucgenalanbul(enteredLength,enteredHeight)
         }
+
+         */
     }
+
+
     private fun setRecyclerview() {
         binding.rv.setHasFixedSize(true)
         binding.rv.layoutManager=LinearLayoutManager(this)
--- Demo/app/src/main/java/com/example/demo/ViewModel.kt
+++ Demo/app/src/main/java/com/example/demo/ViewModel.kt
@@ -1,15 +1,22 @@
 package com.example.demo
 
+import android.text.TextWatcher
+import android.widget.EditText
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.util.ArrayList
+
 class ViewModel:ViewModel() {
-    var sonuc = MutableLiveData<String>()
+    //var sonuc = MutableLiveData<String>()
     var sekil = MutableLiveData<List<Sekiller>>()
     var sekillerList = arrayListOf<Sekiller>()
 
-    init {
+   /* init {
         sonuc = MutableLiveData<String>("0")
     }
+
+    */
+    /*
     fun dortgenalanbul(enteredLength:String,enteredHeight:String){
         val length = enteredLength.toInt()
         val height = enteredHeight.toInt()
@@ -22,15 +29,27 @@ class ViewModel:ViewModel() {
         val triangleArea = length * height / 2
         sonuc.value = triangleArea.toString()
     }
+
+     */
     fun refreshData(){
         val s1 = Sekiller(1,"Square")
         val s2 = Sekiller(2,"Rectangle")
         val s3 = Sekiller(3,"Triangle")
         sekillerList = arrayListOf<Sekiller>(s1,s2,s3)
         sekil.value = sekillerList
+
     }
+
     fun addsekil(sekiller: Sekiller){
         sekillerList.add(sekiller)
         sekil.value = sekillerList
     }
 }
+
+
+
+
+
+
+
+
